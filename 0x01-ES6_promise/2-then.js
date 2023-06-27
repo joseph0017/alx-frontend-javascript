52522378abc20d759a2df5bf3b22b1ccc65b4e04
joseph0017 <josephogiku96@gmail.com>
2023-06-27 09:52:05 -0700

    promise fulfilled3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,16 +5,10 @@ function handleResponseFromAPI(promise) {
   };
   const errMessage = Error();
   const successMessage = 'Got a response from the API';
-  return new Promise((resolve, error) => {
-    if (promise) {
-      resolve(dataObject);
-    } else {
-      error(errMessage);
-    }
-  })
-    .then(() => {
-      console.log(successMessage);
-    });
+  return promise
+    .then(() => dataObject)
+    .catch(() => errMessage)
+    .finally(() => successMessage);
 }
 
 export default handleResponseFromAPI;
