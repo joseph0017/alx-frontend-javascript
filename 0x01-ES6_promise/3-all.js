7f8b48704f3dfe6c76a5ba8dcfafd1c1f594d82f
joseph0017 <josephogiku96@gmail.com>
2023-06-27 10:47:11 -0700

    multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  const errMessage = Error('Signup system offline');
+  const userPhoto = uploadPhoto();
+  const user = createUser();
+  return Promise.all([userPhoto, user])
+    .then((result) => {
+      const photoResponse = result[0];
+      const userResponse = result[1];
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => console.log(errMessage));
+}
+
+export default handleProfileSignup;
+
+handleProfileSignup();
