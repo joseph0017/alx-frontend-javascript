0508dce5585d2f7bf320147360f1cc7e66910844
joseph0017 <josephogiku96@gmail.com>
2023-06-27 09:44:51 -0700

    promise fulfilled

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,20 @@
+function handleResponseFromAPI(promise) {
+  const dataObject = {
+    status: 200,
+    body: 'success',
+  };
+  const errMessage = Error('');
+  const successMessage = 'Got a response from the API';
+  return new Promise((resolve, error) => {
+    if (promise) {
+      resolve(dataObject);
+    } else {
+      error(errMessage);
+    }
+  })
+    .then(() => {
+      console.log(successMessage);
+    });
+}
+
+export default handleResponseFromAPI;
