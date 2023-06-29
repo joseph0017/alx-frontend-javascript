8c0d43202a1975a725aae0db88322196c06be8d1
joseph0017 <josephogiku96@gmail.com>
2023-06-29 09:00:36 -0700

    override car method

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,12 @@
+import Car from "./10-car";
+
+export default class EVCar extends Car {
+  constructor(range, brand, motor, color) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car;
+  }
+}
