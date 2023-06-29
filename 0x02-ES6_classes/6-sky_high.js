fe3f79459bb78d93dc4ebc0dbb7c8bab790e2d6b
joseph0017 <josephogiku96@gmail.com>
2023-06-29 06:27:21 -0700

    sky high

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    this._floors = floors;
+  }
+
+  get floors () {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    const message = `Evacuate slowly the ${this.floors} floors.`;
+    return message;
+  }
+}
