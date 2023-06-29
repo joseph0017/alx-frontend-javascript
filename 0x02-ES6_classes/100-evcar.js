2be7d4a20be737702c0dfade07cdb2a9bcaa5e86
joseph0017 <josephogiku96@gmail.com>
2023-06-29 09:37:53 -0700

    override car method9

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,7 +6,8 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  // eslint-disable-next-line
+  cloneCar() {
+    return new Car();
   }
 }
