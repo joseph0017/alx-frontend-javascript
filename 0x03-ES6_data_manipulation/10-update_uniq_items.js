45ece72e5a22c3fcc30e7972c06c8f74f569bc8b
joseph0017 <josephogiku96@gmail.com>
2023-07-04 08:11:12 -0700

    mappa

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(mappa) {
+  if (!(mappa instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  mappa.forEach((value, key) => {
+    if (value === 1) {
+      mappa.set(key, 100);
+    }
+  });
+  return mappa;
+}
